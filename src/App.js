6a286359a60b47936fa566e196bcf251be018897
christianonoh <onohchibyk@gmail.com>
2023-05-05 01:41:32 +0100

    Add App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
+import NotMatch from './routes/NotMatch';
+import Category from './routes/Category';
+import HomePage from './routes/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Burger!
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="category" element={<Category />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
