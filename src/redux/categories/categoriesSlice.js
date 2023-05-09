b793c3549e9a84e012afaa40232e28740b0d50cb
christianonoh <onohchibyk@gmail.com>
2023-05-09 16:19:54 +0100

    Add categoriesSlice.js

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: [],
+  reducers: {
+    checkStatus() {
+      return 'Under construction';
+    },
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+export default categoriesSlice.reducer;
